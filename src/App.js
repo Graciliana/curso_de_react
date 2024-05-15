565e53de34bd5fc8fc44d89df62cacd8434070aa
Graciliana Kascher <eagraciliana@gmail.com>
2024-05-14 21:07:52 -0300

    :walking:[feat]: stateful class components (statefull)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-
+import logo from './logo.svg'
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  render() {
+  state = {
+    name: 'Graciliana Kascher',
+    counter: 0
+  };
+  handlePclick = () => {
+    this.setState({ name: 'Junior'});
+  }
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+   render() {
+     const { name, counter } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={this.handlePClick}>
+          {name} {counter}
+        </p>
+        <a
+          onClick={this.handleAClick}
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Este é o link
+        </a>
+      </header>
+    </div>
+  );
 }
-
+}
 
 export default App;
